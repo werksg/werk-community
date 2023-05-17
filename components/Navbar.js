f839ab051cb4e9e1ecf6438ee6d3910ede3397df
Aaron Lim CK <aaronlimckdeveloper@gmail.com>
2023-05-17 17:29:12 +0800

    init project

---
+++ components/Navbar.js
@@ -0,0 +1,34 @@
+export default function Navbar() {
+  return (
+    <header class="supports-backdrop-blur:bg-background/60 sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur">
+      <div class="container flex h-16 items-center justify-between">
+        <div>
+          <div class="mr-4">
+            <a class="mr-6 flex items-center space-x-2" href="/">
+              <img
+                className="w-20"
+                src="https://github.com/werksg/email_signature_pack/blob/main/werkLogo.png?raw=true"
+                alt="werk_logo"
+              />
+            </a>
+          </div>
+        </div>
+
+        <nav class="flex items-center space-x-8 text-sm font-medium">
+          <a
+            class="transition-colors hover:text-foreground/80 text-foreground/60"
+            href="/examples"
+          >
+            FAQ
+          </a>
+          <a
+            class="text-foreground/60 transition-colors hover:text-foreground/80"
+            href="https://github.com/shadcn/ui"
+          >
+            Contact
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
+}
