3dcde8095117e8a261c2a7cb62fe4ef2338699b9
Cytan2000 <tanchiyong00@gmail.com>
2023-05-23 15:35:40 +0800

    config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-    target: 'serverless',
-    generateStaticParams: async function () {
-      return {
-        '/': { page: '/' },
-      };
-    },
+    output: 'export',
+    // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+    // trailingSlash: true,
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
   };
-  
+   
   module.exports = nextConfig;
   
