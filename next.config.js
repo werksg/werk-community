d7a84d6566b4ffeb667c4863bbd514afeead5b7a
Cytan2000 <tanchiyong00@gmail.com>
2023-05-23 15:32:21 +0800

    next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    exportPathMap: async function () {
+    target: 'serverless',
+    generateStaticPaths: async function () {
       return {
         '/': { page: '/' },
       };
@@ -8,3 +9,4 @@ const nextConfig = {
   };
   
   module.exports = nextConfig;
+  
