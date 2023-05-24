bdd761dec6ad32035cf1df988161a2b43be7fc0a
Cytan2000 <tanchiyong00@gmail.com>
2023-05-24 10:47:30 +0800

    update main

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import Navbar from "../components/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Footer from "../components/Footer";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,11 +25,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta property="og:title" content="Werk" />
+        <meta property="og:description" content="Werk Community" />
+        <meta property="og:image" content="https://download.werk.sg/assets/werkApp-321478e1.svg" />
+      </Head>
       <body className={`${inter.className} back-gradient`}>
         <Navbar />
         <main className="container">{children}</main>
       </body>
-      <Footer></Footer>
+      <Footer/>
     </html>
   );
 }
